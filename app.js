917ff208b61ab216b8d1f86339bb5ddc1ed85bd7
Your NamejeffIrvine <jirvine1212@gmail.com>
2020-09-29 17:54:19 -0700

    working basic state

--- app.js
+++ app.js
@@ -6,32 +6,46 @@ const guessesLeft = document.getElementById('guesses-left');
 const userInput = document.getElementById('user-input');
 const image = document.getElementById('image');
 const answer = document.getElementById('answer');
+const resetButton = document.getElementById('reset');
 
 // console.log();
-const guesses = 4;
-const correctNum = Math.ceil(Math.random() * 20);
+let guesses = 4;
+let correctNum = Math.ceil(Math.random() * 20);
+let totalWins = 0;
 console.log(correctNum);
 // console.log(correctAnswer);
 
-guessButton.addEventListener('click', () => {
-// USER INPUT === 
-    const compareNumbersResult = compareNumbers(userInput, correctNum);
-    const userNumber = Number(userInput.value);
 
-    if (compareNumbers(userNumber, correctNum) === 0) {
+
+guessButton.addEventListener('click', () => {
+    guesses--;
+    guessesLeft.textContent = guesses;
+    
+    const userNumber = Number(userInput.value);
+    const compNumResult = compareNumbers(userNumber, correctNum);
+    console.log(compNumResult);
+
+    if (compNumResult === 0) {
         answer.textContent = 'You won!';
     }
-        // if (compareNumbers === -1) {
-        //     guesses--
-        // }
-        // if (compareNumbers === 1) {
-        //     guess--
-        // }
-  
-
-
-
+    if (compNumResult === 1) {
+        answer.textContent = 'Sorry that\'s too high!';
+    }
+    if (compNumResult === -1) {
+        answer.textContent = 'Sorry that is too low!';
+    }
+    if (guesses === 0) {
+        guessButton.disabled = true;
+        answer.textContent = 'Sorry you have no more tries!';
+    }
 });
+
+resetButton.addEventListener('click', () => {
+    correctNum = Math.ceil(Math.random() * 20);
+    guesses = 4;
+    answer.textContent = 'The game has reset!';
+});
+
 
 // initialize state
 
--- utils.js
+++ utils.js
@@ -6,8 +6,8 @@ export function compareNumbers(guessNum, correctNum) {
     if (guessNum === correctNum) {
         return 0;
     } else if (guessNum < correctNum) {
-        return 1;
-    } else if (guessNum > correctNum) {
         return -1;
+    } else if (guessNum > correctNum) {
+        return 1;
     }
 }
